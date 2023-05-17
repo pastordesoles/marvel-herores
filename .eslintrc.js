d47556741f022d7c0ef575475337983115985c57
Xavier Sans <xavi.sans.b@gmail.com>
2023-05-17 14:52:02 +0200

    Add husky hooks and eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,49 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['xo', 'prettier'],
+  overrides: [
+    {
+      rules: {
+        '@typescript-eslint/consistent-type-definitions': 'off',
+        '@typescript-eslint/naming-convention': 'off',
+        '@typescript-eslint/consistent-indexed-object-style': 'off',
+        '@typescript-eslint/promise-function-async': 'off',
+        '@typescript-eslint/ban-types': 'off',
+      },
+      extends: ['xo-typescript', 'prettier'],
+      files: ['*.ts', '*.tsx'],
+    },
+    {
+      files: [
+        'src/hooks/useUser/useUser.test.ts',
+        'src/hooks/useStorage/useStorage.test.ts',
+      ],
+      rules: {
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off',
+        '@typescript-eslint/await-thenable': 'off',
+        '@typescript-eslint/no-confusing-void-expression': 'off',
+      },
+    },
+    {
+      files: ['src/store/contexts/CachedRequestsProvider.tsx'],
+      rules: {
+        '@typescript-eslint/no-floating-promises': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        'no-negated-condition': 'off',
+        'no-prototype-builtins': 'off',
+        'no-async-promise-executor': 'off',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
 };
