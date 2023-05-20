18741aa1852ce2799b0b93cf571eadfccfe6736d
Xavier Sans <xavi.sans.b@gmail.com>
2023-05-20 17:28:33 +0200

    Implement home screen route

--- src/Screens/HomeScreen/HomeScreen.tsx
+++ src/Screens/HomeScreen/HomeScreen.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import { View } from 'react-native';
 import globalStyles from '../../styles/globalStyles';
+import { type MarvelHeroData } from '../../proxy/types';
+import HeroesList from '../../components/HeroesList/HeroesList';
+import { useCachedRequests } from '../../proxy/CachedHeroRequestProvider';
 
-const HomeScreen = (): JSX.Element => (
-  <View style={globalStyles.container}>
-    <Text>MARVEL</Text>
-  </View>
-);
+const HomeScreen = (): JSX.Element => {
+  const [state, actions] = useCachedRequests();
+
+  return (
+    <View style={globalStyles.container}>
+      <HeroesList
+        heroesList={state.data as MarvelHeroData}
+        onEndReachedAction={() => {
+          if (!state.isFetching) {
+            actions.paginate();
+          }
+        }}
+        isFetching={state.isFetching}
+      />
+    </View>
+  );
+};
 
 export default HomeScreen;
--- src/navigation/StackNavigator/StackNavigator.tsx
+++ src/navigation/StackNavigator/StackNavigator.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {SafeAreaView, StatusBar} from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaView, StatusBar } from 'react-native';
 import globalStyles from '../../styles/globalStyles';
 import LoginScreen from '../../Screens/LoginScreen/LoginScreen';
 import HomeScreen from '../../Screens/HomeScreen/HomeScreen';
-import {type RootStackParamList} from '../../types/navigation.types';
+import { type RootStackParamList } from '../../types/navigation.types';
 import Routes from './routes';
+import HeroRequestProvider from '../HeroRequestProvider';
+import Header from '../../components/Header/Header';
 
 const StackNavigator = (): JSX.Element => {
   const Stack = createNativeStackNavigator<RootStackParamList>();
 
+  const renderHeader = (): JSX.Element => <Header />;
+
   return (
     <SafeAreaView style={globalStyles.safeArea}>
-      <Stack.Navigator initialRouteName={Routes.login}>
+      <Stack.Navigator
+        initialRouteName={Routes.login}
+        screenOptions={{ header: () => renderHeader() }}
+      >
         <Stack.Screen
           component={LoginScreen}
-          options={{headerShown: false}}
+          options={{ headerShown: false }}
           name={Routes.login}
         />
-        <Stack.Screen component={HomeScreen} name={Routes.home} />
+        <Stack.Screen
+          component={HeroRequestProvider}
+          name={Routes.home}
+          options={{ headerShown: true, gestureEnabled: false }}
+        />
       </Stack.Navigator>
       <StatusBar barStyle={'dark-content'} />
     </SafeAreaView>
