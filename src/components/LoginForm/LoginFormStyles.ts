1a9fa20efed0cb74894304df593810fec5e90c9d
Xavier Sans <xavi.sans.b@gmail.com>
2023-05-20 17:26:19 +0200

    Update Login Form component

--- src/components/LoginForm/LoginFormStyles.ts
+++ src/components/LoginForm/LoginFormStyles.ts
@@ -1,13 +1,16 @@
-import {StyleSheet} from 'react-native';
-import {globalColors} from '../../styles/colors';
+import { StyleSheet } from 'react-native';
+import { globalColors } from '../../styles/colors';
 
 const loginFormStyles = StyleSheet.create({
-  inputContainer: {marginBottom: 14},
+  inputContainer: { marginBottom: 14 },
   input: {
     letterSpacing: 1,
-    borderWidth: 1,
+    borderBottomWidth: 1,
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
     borderColor: globalColors.grey,
-    borderRadius: 8,
+    borderRadius: 4,
     padding: 14,
     fontSize: 18,
     marginBottom: 10,
@@ -17,7 +20,7 @@ const loginFormStyles = StyleSheet.create({
     backgroundColor: globalColors.accent,
     padding: 12,
     alignItems: 'center',
-    borderRadius: 8,
+    borderRadius: 4,
   },
   buttonText: {
     color: globalColors.main,
@@ -26,10 +29,43 @@ const loginFormStyles = StyleSheet.create({
     letterSpacing: 1,
   },
   buttonDisabled: {
-    backgroundColor: globalColors.accent,
+    backgroundColor: globalColors.accentLight,
     padding: 12,
     alignItems: 'center',
     borderRadius: 6,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: globalColors.main,
+    borderRadius: 4,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttonClose: {
+    backgroundColor: globalColors.accent,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
 
