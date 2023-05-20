1b7b141f54bc3ea76c00e4a2f2408f38dd1b7207
Xavier Sans <xavi.sans.b@gmail.com>
2023-05-20 17:24:20 +0200

    Add heroes list

---
+++ src/components/HeroesList/HeroesList.tsx
@@ -0,0 +1,74 @@
+import React, { useRef } from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import HeroCard from '../HeroCard/HeroCard';
+import heroesListStyles from './HeroesListStyles';
+import { globalColors } from '../../styles/colors';
+import { type MarvelHero, type MarvelHeroData } from '../../proxy/types';
+
+interface HeroesListProps {
+  onEndReachedAction: () => void;
+  heroesList: MarvelHeroData;
+  isFetching: boolean;
+}
+
+const renderSeparator = () => <View style={heroesListStyles.gap}></View>;
+
+const HeroesList = ({
+  heroesList,
+  onEndReachedAction,
+  isFetching,
+}: HeroesListProps): JSX.Element => {
+  const heroesListRef = useRef<FlatList<MarvelHero>>(null);
+
+  const scrollToTop = () => {
+    heroesListRef.current!.scrollToOffset({ animated: true, offset: 0 });
+  };
+
+  if (!heroesList?.length) {
+    return (
+      <Text style={heroesListStyles.errorText}>
+        There was an error loading the heroes
+      </Text>
+    );
+  }
+
+  return (
+    <View>
+      <TouchableOpacity
+        style={heroesListStyles.scrollButton}
+        activeOpacity={0.4}
+        onPress={scrollToTop}
+      >
+        <View style={heroesListStyles.arrow} />
+      </TouchableOpacity>
+      <FlatList
+        data={heroesList}
+        renderItem={({ item }) => <HeroCard hero={item} />}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item) => item.id.toString()}
+        onEndReached={onEndReachedAction}
+        onEndReachedThreshold={0.5}
+        ItemSeparatorComponent={renderSeparator}
+        contentContainerStyle={heroesListStyles.list}
+        ref={heroesListRef}
+        bounces={false}
+        scrollEnabled={!isFetching}
+      />
+      {isFetching && heroesList.length ? (
+        <ActivityIndicator
+          size={100}
+          color={globalColors.accent}
+          style={heroesListStyles.loader}
+        />
+      ) : null}
+    </View>
+  );
+};
+
+export default HeroesList;
---
+++ src/components/HeroesList/HeroesListStyles.ts
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+import { globalColors } from '../../styles/colors';
+
+const heroesListStyles = StyleSheet.create({
+  gap: { height: 40 },
+  list: { paddingTop: 20, paddingBottom: 20 },
+  errorText: { fontSize: 20, alignSelf: 'center', letterSpacing: 0.75 },
+  loader: {
+    position: 'absolute',
+    zIndex: 1,
+    borderRadius: 50,
+    bottom: 50,
+    top: 50,
+    left: 50,
+    right: 50,
+  },
+  scrollButton: {
+    height: 50,
+    width: 50,
+    position: 'absolute',
+    zIndex: 1,
+    backgroundColor: globalColors.accent,
+    borderRadius: 4,
+    bottom: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  arrow: {
+    width: 0,
+    height: 0,
+    borderLeftWidth: 10,
+    borderLeftColor: 'transparent',
+    borderRightWidth: 10,
+    borderRightColor: 'transparent',
+    borderBottomWidth: 10,
+    borderBottomColor: 'white',
+  },
+});
+
+export default heroesListStyles;
