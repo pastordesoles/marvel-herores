f9d450e80acaa5c4cc1f8775abccb706f377d16d
Xavier Sans <xavi.sans.b@gmail.com>
2023-05-21 20:44:49 +0200

    Create comiclist component

---
+++ src/components/ComicsList/ComicsListStyles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const comicListStyles = StyleSheet.create({
+  comicsList: { paddingBottom: 20 },
+  gap: { width: 20 },
+  loader: { position: 'absolute', top: -10, right: 10 },
+  error: { fontSize: 16 },
+});
+
+export default comicListStyles;
