468d2a9f0ad74c167edea19564c802339c68019e
Xavier Sans <xavi.sans.b@gmail.com>
2023-05-21 20:42:43 +0200

    Add custom hook to load hero details

---
+++ src/hooks/useLoadCurrentHero/useLoadCurrentHero.ts
@@ -0,0 +1,29 @@
+import { useNavigation } from '@react-navigation/native';
+import { type NavigationProps } from '../../types/navigation.types';
+import { useAppDispatch } from '../../store/hooks';
+import { type HeroStructure } from '../../store/features/heroSlice/types';
+import Routes from '../../navigation/StackNavigator/routes';
+import { comicsEndpoint, marvelBaseUrl } from '../../proxy/authParams';
+import { loadCurrentHeroActionCreator } from '../../store/features/heroSlice/heroSlice';
+
+const useLoadCurrentHero = () => {
+  const dispatch = useAppDispatch();
+  const navigation = useNavigation<NavigationProps>();
+
+  const loadCurrentHero = (selectedHero: HeroStructure) => {
+    navigation.navigate(Routes.detail);
+
+    const selectedHeroComicsUrl = `${marvelBaseUrl}/${selectedHero.id}/${comicsEndpoint}`;
+
+    dispatch(
+      loadCurrentHeroActionCreator({
+        currentHeroDetail: selectedHero,
+        url: selectedHeroComicsUrl,
+      }),
+    );
+  };
+
+  return { loadCurrentHero };
+};
+
+export default useLoadCurrentHero;
