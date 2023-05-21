379c6da8a1dcdb0766d70e440d87c3df8fe13c6a
Xavier Sans <xavi.sans.b@gmail.com>
2023-05-21 20:50:29 +0200

    Add data fetch and styles to screens

--- src/Screens/LoginScreen/LoginScreen.tsx
+++ src/Screens/LoginScreen/LoginScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, Text, View} from 'react-native';
+import { Image, View } from 'react-native';
 import loginScreenStyles from './LoginScreenStyles';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import globalStyles from '../../styles/globalStyles';
